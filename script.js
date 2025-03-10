cc6f5e76e8a36dc7558360eb71aaa5b860fefd6c
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-10 11:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your code here
+const userImage = document.getElementById("userImage");
+const userName = document.getElementById("userName");
+const additionalInfo = document.getElementById("additionalInfo");
+const getUserBtn = document.getElementById("getUser");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentUser = {};
+
+async function fetchUser(){
+	try {
+		const response = await fetch("https://randomuser.me/api/");
+		const data = await response.json();
+		const user = data.results[0];
+
+		currentUser = {
+            name: `${user.name.first} ${user.name.last}`,
+            image: user.picture.large,
+            age: user.dob.age,
+            email: user.email,
+            phone: user.phone
+        };
+
+		userImage.src = currentUser.image;
+		userName.textContent = currentUser.name;
+		additionalInfo.innerHTML = ""
+		
+	} catch (error) {
+		console.error("Error fetching user:", error);
+	}
+}
+
+buttons.forEach(button=>{
+	button.addEventListener("click",(e)=>{
+		additionalInfo.textContent = ""
+		const attribute = e.target.getAttribute("data-attr");
+		additionalInfo.textContent = currentUser[attribute]
+	})
+})
+
+getUserBtn.addEventListener("click",fetchUser)
+
+fetchUser();
